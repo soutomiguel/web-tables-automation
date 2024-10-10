b97242c1bc9c5a4bef302811f1a124b5a71fb28d
miguel <miguelalesouto@gmail.com>
2024-10-10 19:48:08 -0300

    Initial project refactoring completed

--- conftest.py
+++ conftest.py
@@ -1,10 +1,29 @@
 from selenium import webdriver
 import pytest
+import json
+import os
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--env",
+        action = "store",
+        default = "prod",
+        help = "Select environments using dev, stage or prod"
+    )
 
 @pytest.fixture
-def driver():
+def environment(request):
+    env = request.config.getoption("--env")
+    env_file = os.path.join(os.path.dirname(__file__), 'envs', f'{env}.json')
+
+    with open(env_file, 'r') as file:
+        config = json.load(file)
+        return config
+
+@pytest.fixture
+def driver(environment):
     driver = webdriver.Chrome()
     driver.maximize_window()
-    driver.get("https://demoqa.com/webtables")
+    driver.get(environment["base_url"])
     yield driver
     driver.close()
--- pages/base_page.py
+++ pages/base_page.py
@@ -7,16 +7,16 @@ class BasePage:
         self.driver = driver
         self.timeout = 10
 
-    def get_element(self, element):
+    def __get_element(self, element):
         try:
             return WebDriverWait(self.driver, self.timeout)\
                 .until(EC.element_to_be_clickable(element))
         except TimeoutException:
-            return f'Element not found'
+            print('Element not found')
 
-    def click(self, element):
-        element_to_be_clicked = self.get_element(element)
+    def __click(self, element):
+        element_to_be_clicked = self.__get_element(element)
         if element_to_be_clicked:
             element_to_be_clicked.click()
         else:
-            print(f'Element could not be clicked')
+            print('Element could not be clicked')
